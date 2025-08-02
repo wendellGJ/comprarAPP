2067753a2f8116dcd1b3ab7084a9b210a6f14804
Wendell Guimaraes Jr <junior.wendell@hotmail.com>
2025-08-02 18:48:02 -0300

    feat: initial struct

---
+++ Comprar/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+import { styles} from "./src/app/Home/styles"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  )
+}
---
+++ Comprar/src/app/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Image, View } from "react-native"
+
+import { styles } from "./styles"
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+    </View>
+  )
+}
